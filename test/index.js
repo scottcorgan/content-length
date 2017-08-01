40d56385634856727731b131eb9426fac2da230a
Scott Corgan <scottcorgan@gmail.com>
2017-08-01 09:29:28 -0400

    Fix double content length
    Fixes #1

--- test/index.js
+++ test/index.js
@@ -7,15 +7,15 @@ describe('content length middleware', function () {
   it('gives the content length in a callback', function (done) {
     var app = express();
     var responseLength = 0;
-    
+
     app.use(contentLength(function (err, len) {
       responseLength = len;
     }));
-    
+
     app.use(function (req, res) {
       res.send('Hi');
     });
-    
+
     request(app)
       .get('/')
       .expect(function () {
@@ -27,19 +27,19 @@ describe('content length middleware', function () {
         });
       });
   });
-  
+
   it('gives a 0 content length if there is no response', function (done) {
     var app = express();
     var responseLength = 0;
-    
+
     app.use(contentLength(function (err, len) {
       responseLength = len;
     }));
-    
+
     app.use(function (req, res) {
       res.send();
     });
-    
+
     request(app)
       .get('/')
       .expect(function () {
@@ -51,18 +51,18 @@ describe('content length middleware', function () {
         });
       });
   });
-  
+
   it('throws an error if no callback is given', function () {
     var app = express();
-    
+
     expect(function () {
       app.use(contentLength());
     }).to.throw();
   });
-  
+
   it('throws an error if the callback is not a function', function () {
     var app = express();
-    
+
     expect(function () {
       app.use(contentLength('test'));
     }).to.throw();
